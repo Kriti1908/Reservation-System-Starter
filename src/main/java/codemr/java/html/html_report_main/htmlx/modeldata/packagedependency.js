ecba4fa380761aa2c10d2565e23966e3caa5d900
Janya Gupta <janya.gupta@students.iiit.ac.in>
2026-03-04 18:17:05 +0530

    Added designite before and after metrics

---
+++ src/main/java/codemr/java/html/html_report_main/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,20 @@
+var matrix = [[0,0,0,0,1,0],[0,0,0,1,0,0],[0,0,0,0,0,0],[0,2,1,0,0,0],[0,2,0,0,0,1],[0,0,0,0,0,0]]
+var packages = [{
+"name": " ", "color": " #3182bd"
+}
+,{
+"name": " flight.reservation", "color": " #6baed6"
+}
+,{
+"name": " flight.reservation.payment", "color": " #9ecae1"
+}
+,{
+"name": " flight.reservation.order", "color": " #c6dbef"
+}
+,{
+"name": " flight.reservation.flight", "color": " #e6550d"
+}
+,{
+"name": " flight.reservation.plane", "color": " #fd8d3c"
+}
+];
